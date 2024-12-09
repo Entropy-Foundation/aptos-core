67aed6b80a66a2cda761742392c2b8fa93a2b1d5
Aregnaz Harutyunyan <>
2024-12-09 13:12:05 +0400

    Fixec copywrite and *.md File source
    - Updated move files to avoid *.md files regeneration
    - Updated some *.md files to replicate the latest version of the sources

--- storage/backup/backup-cli/src/backup_types/transaction/analysis.rs
+++ storage/backup/backup-cli/src/backup_types/transaction/analysis.rs
@@ -1,8 +1,7 @@
+// Copyright (c) 2024 Supra.
 // Copyright © Aptos Foundation
 // Parts of the project are originally copyright © Meta Platforms, Inc.
 // SPDX-License-Identifier: Apache-2.0
-//
-// Copyright (c) 2024 Supra.
 
 use anyhow::Result;
 use aptos_types::{
