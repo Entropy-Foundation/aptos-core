3d6c06eccc76fa40cd915673cf1183e91c346cf4
Aregnaz Harutyunyan <89187359+aregng@users.noreply.github.com>
2024-12-30 17:37:34 +0400

    [AN-Issue-1347] Implemented Automated transaction execution flow (#136)
    * [AN-Issue-1347] Implemented Automated transasction execution flow
    
    * Updated automated task prologue and added e2e tests
    
    - Added chain-id verification and task availability check by task index
    - Added e2e tests for AutoamtedTransaction
    
    * Fixed test failure
    
    * Added task sender check in automated_transaction_prologue
    
    ---------
    
    Co-authored-by: Aregnaz Harutyunyan <>

--- third_party/move/move-core/types/src/vm_status.rs
+++ third_party/move/move-core/types/src/vm_status.rs
@@ -593,6 +593,8 @@ pub enum StatusCode {
     RESERVED_VALIDATION_ERROR_7 = 42,
     RESERVED_VALIDATION_ERROR_8 = 43,
     RESERVED_VALIDATION_ERROR_9 = 44,
+    // Failed to identify active automated task by provided index/sequence-number
+    NO_ACTIVE_AUTOMATED_TASK = 45,
 
     // When a code module/script is published it is verified. These are the
     // possible errors that can arise from the verification process.
@@ -732,6 +734,8 @@ pub enum StatusCode {
     // Verification errors related to automation registration transaction
     // Validation of the entry function to be automated failed.
     INVALID_AUTOMATION_INNER_PAYLOAD = 1132,
+    // Automated transaction validation failures
+    INVALID_AUTOMATED_PAYLOAD = 1133,
 
 
     // These are errors that the VM might raise if a violation of internal
