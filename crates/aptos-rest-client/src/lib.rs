439cf3366aefd2b97f6a7a5c3b3231e7462fb956
kiran-supra <k.kummari@supraoracles.com>
2025-02-10 13:43:13 +0530

    refactor submit function

--- crates/aptos-rest-client/src/lib.rs
+++ crates/aptos-rest-client/src/lib.rs
@@ -47,7 +47,7 @@ use reqwest::{
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use serde_json::{json, Value};
 pub use state::State;
-use std::{collections::BTreeMap, fs::File, future::Future, time::Duration};
+use std::{collections::BTreeMap, future::Future, time::Duration};
 use tokio::time::Instant;
 pub use types::{deserialize_from_prefixed_hex_string, Account, Resource};
 use url::Url;
@@ -449,8 +449,8 @@ impl Client {
         let txn_payload = Move::from(txn.clone());
         let url = self.build_path("rpc/v1/transactions/submit")?;
 
-        let mut file= File::create("gk.json").unwrap();
-        serde_json::to_writer_pretty(&mut file, &txn_payload).unwrap();
+        // let mut file= File::create("gk.json").unwrap();
+        // serde_json::to_writer_pretty(&mut file, &txn_payload).unwrap();
 
         let response = self
             .inner
