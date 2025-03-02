4f647382c4c68d9c5f0df8f5af53dad596b1f0de
nizam-supraoracles <n.ansari@supraoracles.com>
2025-03-02 18:10:03 +0530

    add `congestion_exponent` field in AutomationRegistryConfigV1 struct in rust layer

--- types/src/on_chain_config/automation_registry.rs
+++ types/src/on_chain_config/automation_registry.rs
@@ -12,6 +12,7 @@ const DEFAULT_AUTOMATION_BASE_FEE_IN_QUANTS_PER_SEC: u64 = 1000;
 const ONE_SUPRA_IN_QUANTS: u64 = 100_000_000;
 const DEFAULT_CONGESTION_THRESHOLD_PERCENTAGE: u8 = 80;
 const DEFAULT_CONGESTION_BASE_FEE_IN_QUANTS_PER_SEC: u64 = 100;
+const DEFAULT_CONGESTION_EXPONENT: u64 = 6;
 
 /// Initial version of configuration parameters for Supra native automation feature
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize, Eq)]
@@ -32,6 +33,8 @@ pub struct AutomationRegistryConfigV1 {
     congestion_threshold_percentage: u8,
     /// Base fee per second for the full capacity of the automation registry when the congestion threshold is exceeded.
     congestion_base_fee_in_quants_per_sec: u64,
+    /// The congestion fee increases exponentially based on this value, ensuring higher fees as the registry approaches full capacity.
+    congestion_exponent: u64,
 }
 
 impl Default for AutomationRegistryConfigV1 {
@@ -43,6 +46,7 @@ impl Default for AutomationRegistryConfigV1 {
             flat_registration_fee_in_quants: ONE_SUPRA_IN_QUANTS,
             congestion_threshold_percentage: DEFAULT_CONGESTION_THRESHOLD_PERCENTAGE,
             congestion_base_fee_in_quants_per_sec: DEFAULT_CONGESTION_BASE_FEE_IN_QUANTS_PER_SEC,
+            congestion_exponent: DEFAULT_CONGESTION_EXPONENT,
         }
     }
 }
@@ -68,6 +72,10 @@ impl AutomationRegistryConfigV1 {
     }
     pub fn congestion_base_fee_in_quants_per_sec(&self) -> u64 {
         self.congestion_base_fee_in_quants_per_sec
+    }
+
+    pub fn congestion_exponent(&self) -> u64 {
+        self.congestion_exponent
     }
 
     pub fn verify(&self) {
@@ -100,6 +108,7 @@ impl AutomationRegistryConfig {
             MoveValue::U64(config.flat_registration_fee_in_quants()),
             MoveValue::U8(config.congestion_threshold_percentage()),
             MoveValue::U64(config.congestion_base_fee_in_quants_per_sec()),
+            MoveValue::U64(config.congestion_exponent()),
         ];
         serialize_values(&arguments)
     }
