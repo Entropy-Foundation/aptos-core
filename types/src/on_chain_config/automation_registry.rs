b3a2ed522f0391ce7d44a837ff9c7afe69dd50c9
Aregnaz Harutyunyan <>
2025-08-20 10:07:05 +0400

    Flattened AutomationRegistryConfigV2 and used derive-getters to avoid manual code generation

--- types/src/on_chain_config/automation_registry.rs
+++ types/src/on_chain_config/automation_registry.rs
@@ -1,12 +1,13 @@
 // Copyright (c) 2025 Supra.
 // SPDX-License-Identifier: Apache-2.0
 
-use std::ops::Deref;
+use crate::on_chain_config::OnChainConfig;
+use derive_getters::Getters;
+use derive_more::Constructor;
 use move_core_types::account_address::AccountAddress;
 use move_core_types::value::{serialize_values, MoveValue};
-use serde::{Deserialize, Serialize};
-use crate::on_chain_config::OnChainConfig;
 use move_core_types::{ident_str, identifier::IdentStr, move_resource::MoveStructType};
+use serde::{Deserialize, Serialize};
 
 const ONE_MONTH_IN_SECS: u64 = 2_626_560;
 const DEFAULT_REGISTRY_MAX_GAS_CAP: u64 = 100_000_000;
@@ -19,7 +20,7 @@ const DEFAULT_TASK_CAPACITY: u16 = 500;
 const DEFAULT_CYCLE_DURATION_SECS: u64 = 1200;
 
 /// Initial version of configuration parameters for Supra native automation feature
-#[derive(Clone, Debug, Deserialize, PartialEq, Serialize, Eq)]
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize, Eq, Getters, Constructor)]
 pub struct AutomationRegistryConfigV1 {
     /// Maximum allowable duration (in seconds) from the registration time that an automation task can run.
     /// If the expiration time exceeds this duration, the task registration will fail.
@@ -41,7 +42,6 @@ pub struct AutomationRegistryConfigV1 {
     congestion_exponent: u8,
     /// Maximum number of tasks that registry can hold.
     task_capacity: u16,
-
 }
 
 impl Default for AutomationRegistryConfigV1 {
@@ -59,133 +59,74 @@ impl Default for AutomationRegistryConfigV1 {
     }
 }
 
-impl AutomationRegistryConfigV1 {
-
-    pub fn new(
-        task_duration_cap_in_secs: u64,
-        registry_max_gas_cap: u64,
-        automation_base_fee_in_quants_per_sec: u64,
-        flat_registration_fee_in_quants: u64,
-        congestion_threshold_percentage: u8,
-        congestion_base_fee_in_quants_per_sec: u64,
-        congestion_exponent: u8,
-        task_capacity: u16,
-
-    ) -> Self {
-        Self {
-            task_duration_cap_in_secs,
-            registry_max_gas_cap,
-            automation_base_fee_in_quants_per_sec,
-            flat_registration_fee_in_quants,
-            congestion_threshold_percentage,
-            congestion_base_fee_in_quants_per_sec,
-            congestion_exponent,
-            task_capacity,
-        }
-    }
-
-    pub fn task_duration_cap_in_secs(&self) -> u64 {
-        self.task_duration_cap_in_secs
-    }
-
-    pub fn registry_max_gas_cap(&self) -> u64 {
-        self.registry_max_gas_cap
-    }
-
-    pub fn automation_base_fee_in_quants_per_sec(&self) -> u64 {
-        self.automation_base_fee_in_quants_per_sec
-    }
-    pub fn flat_registration_fee_in_quants(&self) -> u64 {
-        self.flat_registration_fee_in_quants
-    }
-
-    pub fn congestion_threshold_percentage(&self) -> u8 {
-        self.congestion_threshold_percentage
-    }
-    pub fn congestion_base_fee_in_quants_per_sec(&self) -> u64 {
-        self.congestion_base_fee_in_quants_per_sec
-    }
-
-    pub fn congestion_exponent(&self) -> u8 {
-        self.congestion_exponent
-    }
-
-    pub fn task_capacity(&self) -> u16 {
-        self.task_capacity
-    }
-
-}
-
 /// Extended version of configuration parameters for Supra native automation feature supporting cycle-duration.
-#[derive(Clone, Debug, Deserialize, PartialEq, Serialize, Eq)]
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize, Eq, Getters, Constructor)]
 pub struct AutomationRegistryConfigV2 {
-    /// Main user task registration related parameters.
-    main: AutomationRegistryConfigV1,
+    /// Maximum allowable duration (in seconds) from the registration time that an automation task can run.
+    /// If the expiration time exceeds this duration, the task registration will fail.
+    task_duration_cap_in_secs: u64,
+    /// Maximum gas allocation for automation tasks per epoch
+    /// Exceeding this limit during task registration will cause failure and is used in fee calculation.
+    registry_max_gas_cap: u64,
+    /// Base fee per second for the full capacity of the automation registry, measured in quants/sec.
+    /// The capacity is considered full if the total committed gas of all registered tasks equals registry_max_gas_cap.
+    automation_base_fee_in_quants_per_sec: u64,
+    /// Flat registration fee charged by default for each task.
+    flat_registration_fee_in_quants: u64,
+    /// Ratio (in the range [0;100]) representing the acceptable upper limit of committed gas amount
+    /// relative to registry_max_gas_cap. Beyond this threshold, congestion fees apply.
+    congestion_threshold_percentage: u8,
+    /// Base fee per second for the full capacity of the automation registry when the congestion threshold is exceeded.
+    congestion_base_fee_in_quants_per_sec: u64,
+    /// The congestion fee increases exponentially based on this value, ensuring higher fees as the registry approaches full capacity.
+    congestion_exponent: u8,
+    /// Maximum number of tasks that registry can hold.
+    task_capacity: u16,
     /// Cycle duration in seconds.
     cycle_duration_secs: u64,
 }
 
-
 impl Default for AutomationRegistryConfigV2 {
     fn default() -> Self {
         Self {
-            main: Default::default(),
+            task_duration_cap_in_secs: ONE_MONTH_IN_SECS,
+            registry_max_gas_cap: DEFAULT_REGISTRY_MAX_GAS_CAP,
+            automation_base_fee_in_quants_per_sec: DEFAULT_AUTOMATION_BASE_FEE_IN_QUANTS_PER_SEC,
+            flat_registration_fee_in_quants: ONE_SUPRA_IN_QUANTS,
+            congestion_threshold_percentage: DEFAULT_CONGESTION_THRESHOLD_PERCENTAGE,
+            congestion_base_fee_in_quants_per_sec: DEFAULT_CONGESTION_BASE_FEE_IN_QUANTS_PER_SEC,
+            congestion_exponent: DEFAULT_CONGESTION_EXPONENT,
+            task_capacity: DEFAULT_TASK_CAPACITY,
             cycle_duration_secs: DEFAULT_CYCLE_DURATION_SECS,
         }
-    }
-}
-
-impl AutomationRegistryConfigV2 {
-    pub fn cycle_duration_secs(&self) -> u64 {
-        self.cycle_duration_secs
-    }
-
-    pub fn new(
-        task_duration_cap_in_secs: u64,
-        registry_max_gas_cap: u64,
-        automation_base_fee_in_quants_per_sec: u64,
-        flat_registration_fee_in_quants: u64,
-        congestion_threshold_percentage: u8,
-        congestion_base_fee_in_quants_per_sec: u64,
-        congestion_exponent: u8,
-        task_capacity: u16,
-        cycle_duration_secs: u64,
-
-    ) -> Self {
-        let v1= AutomationRegistryConfigV1::new(
-            task_duration_cap_in_secs,
-            registry_max_gas_cap,
-            automation_base_fee_in_quants_per_sec,
-            flat_registration_fee_in_quants,
-            congestion_threshold_percentage,
-            congestion_base_fee_in_quants_per_sec,
-            congestion_exponent,
-            task_capacity,
-        );
-        Self {
-            main: v1,
-            cycle_duration_secs,
-        }
-    }
-}
-
-impl Deref for AutomationRegistryConfigV2 {
-    type Target = AutomationRegistryConfigV1;
-
-    fn deref(&self) -> &Self::Target {
-        &self.main
     }
 }
 
 impl From<AutomationRegistryConfigV1> for AutomationRegistryConfigV2 {
     fn from(v1: AutomationRegistryConfigV1) -> Self {
+        let AutomationRegistryConfigV1 {
+            task_duration_cap_in_secs,
+            registry_max_gas_cap,
+            automation_base_fee_in_quants_per_sec,
+            flat_registration_fee_in_quants,
+            congestion_threshold_percentage,
+            congestion_base_fee_in_quants_per_sec,
+            congestion_exponent,
+            task_capacity,
+        } = v1;
         Self {
-            main: v1,
+            task_duration_cap_in_secs,
+            registry_max_gas_cap,
+            automation_base_fee_in_quants_per_sec,
+            flat_registration_fee_in_quants,
+            congestion_threshold_percentage,
+            congestion_base_fee_in_quants_per_sec,
+            congestion_exponent,
+            task_capacity,
             cycle_duration_secs: DEFAULT_CYCLE_DURATION_SECS,
         }
     }
 }
-
 
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize, Eq)]
 pub enum AutomationRegistryConfig {
@@ -208,31 +149,31 @@ impl AutomationRegistryConfig {
             AutomationRegistryConfig::V1(config) => {
                 vec![
                     MoveValue::Signer(signer_address),
-                    MoveValue::U64(config.task_duration_cap_in_secs()),
-                    MoveValue::U64(config.registry_max_gas_cap()),
-                    MoveValue::U64(config.automation_base_fee_in_quants_per_sec()),
-                    MoveValue::U64(config.flat_registration_fee_in_quants()),
-                    MoveValue::U8(config.congestion_threshold_percentage()),
-                    MoveValue::U64(config.congestion_base_fee_in_quants_per_sec()),
-                    MoveValue::U8(config.congestion_exponent()),
-                    MoveValue::U16(config.task_capacity()),
+                    MoveValue::U64(*config.task_duration_cap_in_secs()),
+                    MoveValue::U64(*config.registry_max_gas_cap()),
+                    MoveValue::U64(*config.automation_base_fee_in_quants_per_sec()),
+                    MoveValue::U64(*config.flat_registration_fee_in_quants()),
+                    MoveValue::U8(*config.congestion_threshold_percentage()),
+                    MoveValue::U64(*config.congestion_base_fee_in_quants_per_sec()),
+                    MoveValue::U8(*config.congestion_exponent()),
+                    MoveValue::U16(*config.task_capacity()),
                     MoveValue::U64(DEFAULT_CYCLE_DURATION_SECS),
                 ]
-            }
+            },
             AutomationRegistryConfig::V2(config) => {
                 vec![
                     MoveValue::Signer(signer_address),
-                    MoveValue::U64(config.task_duration_cap_in_secs()),
-                    MoveValue::U64(config.registry_max_gas_cap()),
-                    MoveValue::U64(config.automation_base_fee_in_quants_per_sec()),
-                    MoveValue::U64(config.flat_registration_fee_in_quants()),
-                    MoveValue::U8(config.congestion_threshold_percentage()),
-                    MoveValue::U64(config.congestion_base_fee_in_quants_per_sec()),
-                    MoveValue::U8(config.congestion_exponent()),
-                    MoveValue::U16(config.task_capacity()),
-                    MoveValue::U64(config.cycle_duration_secs()),
+                    MoveValue::U64(*config.task_duration_cap_in_secs()),
+                    MoveValue::U64(*config.registry_max_gas_cap()),
+                    MoveValue::U64(*config.automation_base_fee_in_quants_per_sec()),
+                    MoveValue::U64(*config.flat_registration_fee_in_quants()),
+                    MoveValue::U8(*config.congestion_threshold_percentage()),
+                    MoveValue::U64(*config.congestion_base_fee_in_quants_per_sec()),
+                    MoveValue::U8(*config.congestion_exponent()),
+                    MoveValue::U16(*config.task_capacity()),
+                    MoveValue::U64(*config.cycle_duration_secs()),
                 ]
-            }
+            },
         };
         serialize_values(&arguments)
     }
@@ -257,7 +198,7 @@ pub enum AutomationCycleState {
     READY = 0,
     STARTED,
     FINISHED,
-    SUSPENDED
+    SUSPENDED,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize, Default)]
@@ -306,7 +247,7 @@ pub struct AutomationCycleTransitionState {
     pub expected_tasks_to_be_processed: Vec<u64>,
     /// Position of the task index in the expected_tasks_to_be_processed to be processed next.
     /// It is incremented when an expected task is successfully processed.
-    pub next_task_index_position: u64
+    pub next_task_index_position: u64,
 }
 
 /// On-chain Automation Cycle Details.
@@ -339,4 +280,3 @@ impl OnChainConfig for AutomationCycleDetails {
     const MODULE_IDENTIFIER: &'static str = "automation_registry";
     const TYPE_IDENTIFIER: &'static str = "AutomationCycleDetails";
 }
-
