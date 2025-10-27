287160ec980c31eba1a445eeec3db3548c669894
Aregnaz Harutyunyan <>
2025-10-27 20:57:05 +0400

    Addressed review comment

--- aptos-move/aptos-gas-schedule/src/gas_schedule/aptos_framework.rs
+++ aptos-move/aptos-gas-schedule/src/gas_schedule/aptos_framework.rs
@@ -334,6 +334,6 @@ crate::gas_schedule::macros::define_gas_parameters!(
         [string_utils_per_byte: InternalGasPerByte, { 8.. =>"string_utils.format.per_byte" }, 3],
 
         // Gas for vector utils. Referenced the value of vector_swap_base from instr.rs
-        [vector_utils_per_swap: InternalGasPerByte, { RELEASE_V1_16_SUPRA_V1_7_14.. =>"vector_utils.format.per_swap" }, 1102],
+        [vector_utils_per_swap: InternalGasPerByte, { RELEASE_V1_16_SUPRA_V1_7_14.. => "vector_utils.format.per_swap" }, 1102],
     ]
 );
